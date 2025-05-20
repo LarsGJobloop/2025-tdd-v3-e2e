a0f7f91cb1f25ed20ac6bf39238efb3b2e3ef3fb
zabronax <zabronax@gmail.com>
2025-05-20 11:06:21 +0200

    setup anemic domain model

---
+++ TaskManager/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManager;
+
+public class TaskManager
+{
+  public List<Task> Tasks = new List<Task>();
+
+  public Task AddTask(string title)
+  {
+    var newTask = new Task();
+    newTask.Title = title;
+    newTask.Complete = false;
+    newTask.CompletedAt = null;
+
+    Tasks.Add(newTask);
+    return newTask;
+  }
+}
---
+++ TaskManager/Tasks.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager;
+
+public class Task
+{
+  public string? Title { get; set; }
+  public bool? Complete { get; set; }
+  public DateTime? CompletedAt { get; set; }
+}
